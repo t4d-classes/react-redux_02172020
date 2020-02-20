74368da3d18b02bf80412b6a2e6ec46e7c7c9ac3
Eric Greene <eric@t4d.io>
2020-02-20 11:18:50 -0600

    add code demos

--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
 
-// import { calcToolStore } from './stores/calcToolStore';
-// import { CalcToolContainer } from './containers/CalcToolContainer';
+// // import { calcToolStore } from './stores/calcToolStore';
+// // import { CalcToolContainer } from './containers/CalcToolContainer';
+
+// // ReactDOM.render(
+// //   <Provider store={calcToolStore}>
+// //     <CalcToolContainer />
+// //   </Provider>,
+// //   document.querySelector('#root'),
+// // );
+
+// import { carToolStore } from './stores/carToolStore';
+// import { CarToolContainer } from './containers/CarToolContainer';
 
 // ReactDOM.render(
-//   <Provider store={calcToolStore}>
-//     <CalcToolContainer />
+//   <Provider store={carToolStore}>
+//     <CarToolContainer />
 //   </Provider>,
 //   document.querySelector('#root'),
 // );
-
-import { carToolStore } from './stores/carToolStore';
-import { CarToolContainer } from './containers/CarToolContainer';
-
-ReactDOM.render(
-  <Provider store={carToolStore}>
-    <CarToolContainer />
-  </Provider>,
-  document.querySelector('#root'),
-);
